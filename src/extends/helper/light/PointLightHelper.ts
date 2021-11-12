63a0abae214e6f555178fb7c1ad67ba0a60de470
Shiotsuki_ <564165781@qq.com>
2021-11-12 10:29:48 +0800

    perf：优化点光辅助

--- src/extends/helper/light/PointLightHelper.ts
+++ src/extends/helper/light/PointLightHelper.ts
@@ -1,18 +1,18 @@
 import { BufferGeometry, Color, Float32BufferAttribute, Intersection, Line, LineBasicMaterial, LineSegments, Mesh, MeshBasicMaterial, Object3D, OctahedronBufferGeometry, PointLight, Points, Raycaster, Sphere, SphereBufferGeometry, Vector3 } from "three";
 import { getHelperLineMaterial } from "../common";
 
-export class PointLightHelper extends Object3D{
+export class PointLightHelper extends LineSegments{
     sphere: Sphere
     target: PointLight
-    scource: LineSegments
     shape: Mesh
     cachaColor: number
     cachaDistance: number
+    type: string = 'VisPointLightHelper'
 
   constructor (pointLight: PointLight) {
     super()
     // 光源
-    const scourceGeometry = new BufferGeometry()
+    this.geometry = new BufferGeometry()
     const points = [
       -1, 0, 0,
       1, 0, 0,
@@ -33,9 +33,8 @@ export class PointLightHelper extends Object3D{
       0.707, 0, -0.707,
       -0.707, 0, 0.707,
     ]
-    scourceGeometry.setAttribute('position', new Float32BufferAttribute(points, 3))
-    const scource = new LineSegments(scourceGeometry, getHelperLineMaterial())
-    scource.raycast = () => {}
+    this.geometry.setAttribute('position', new Float32BufferAttribute(points, 3))
+    this.material = getHelperLineMaterial()
 
     // 形状
     const color = new Color().copy(pointLight.color).multiplyScalar(pointLight.intensity)
@@ -50,22 +49,19 @@ export class PointLightHelper extends Object3D{
 
     this.shape = shape
     this.target = pointLight
-    this.scource = scource
     this.sphere = new Sphere(new Vector3(0, 0, 0), 1)
     this.cachaColor = pointLight.color.getHex()
     this.cachaDistance = pointLight.distance
 
-    this.add(scource)
     this.add(this.shape)
     this.matrixAutoUpdate = false
     this.matrix = pointLight.matrix
-    console.log(this)
 
-    const cacheUpdateWorldMatrix = this.updateMatrixWorld.bind(this)
-    this.updateMatrixWorld = function (focus?: boolean) {
+    // https://github.com/mrdoob/three.js/issues/14970
+    this.onBeforeRender = () => {
       const light = this.target
       const shape = this.shape
-      const scource = this.scource
+      const scource = this
       if (light.distance !== this.cachaDistance) {
         shape.geometry.dispose()
         shape.geometry = new OctahedronBufferGeometry(light.distance, 0)
@@ -76,27 +72,8 @@ export class PointLightHelper extends Object3D{
         (shape.material as MeshBasicMaterial).color.copy(light.color).multiplyScalar(light.intensity);
         (scource.material as LineBasicMaterial).color.copy(light.color).multiplyScalar(light.intensity)
         this.cachaColor = light.color.getHex()
-      }
-      cacheUpdateWorldMatrix(focus)
+      } 
     }
-
-    // https://github.com/mrdoob/three.js/issues/14970
-    // this.onBeforeRender = () => {
-    //   const light = this.target
-    //   const shape = this.shape
-    //   const scource = this.scource
-    //   if (light.distance !== this.cachaDistance) {
-    //     shape.geometry.dispose()
-    //     shape.geometry = new OctahedronBufferGeometry(light.distance, 0)
-    //     this.cachaDistance = light.distance
-    //   }
-
-    //   if (light.color.getHex() !== this.cachaColor) {
-    //     (shape.material as MeshBasicMaterial).color.copy(light.color).multiplyScalar(light.intensity);
-    //     (scource.material as LineBasicMaterial).color.copy(light.color).multiplyScalar(light.intensity)
-    //     this.cachaColor = light.color.getHex()
-    //   } 
-    // }
   }
 
 
