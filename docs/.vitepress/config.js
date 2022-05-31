4e4e1bbd6a1f109f7bee10591ba477b6510a516a
Shiotsuki_ <564165781@qq.com>
2022-05-31 17:47:47 +0800

    feat：doc优化

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,4 +3,15 @@ export default {
   description: "more convenient development for three.js",
   dest: "./dist",
   head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  themeConfig: {
+    nav: [
+      { text: "首页", link: "/" },
+      { text: "关于", link: "/about/" },
+      {
+        text: "github",
+        link: "https://github.com/Shiotsukikaedesari/vis-three",
+      },
+    ],
+    sidebar: [{ text: "我的", link: "/mine/" }],
+  },
 };
