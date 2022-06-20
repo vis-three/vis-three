95709e3eb2bb1f348949759036620b0e304c2991
Shiotsuki_ <564165781@qq.com>
2022-06-20 10:28:28 +0800

    feat： add echarts

---
+++ examples/dataview/index.js
@@ -0,0 +1,12 @@
+export default {
+  router: "dataview",
+  children: ["/Echarts.html"],
+  position: {
+    x: -30,
+    y: -40,
+    z: 0,
+  },
+  rotation: {
+    y: (Math.PI / 180) * 20,
+  },
+};
