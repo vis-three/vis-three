58ba0856ef3fde6664b21bee6081b51430b6faaf
Shiotsuki_ <564165781@qq.com>
2022-07-23 20:25:35 +0800

    feat：website update

--- dist-website/examples/assets/plugin/AxesHelperPlugin.b9ce72d7.js
+++
@@ -1,1 +0,0 @@
-import"../common.006007ed.js";import{f as t}from"../Vis.es.d8d395f3.js";import"../three.fec063a1.js";import"../vis-three.59b4e7ed.js";const s=new t().install("AxesHelper",{length:200}).complete().setDom(document.getElementById("app")).setSize().play();let e=!0;document.getElementById("operate").onclick=n=>{e=!e,s.setAxesHelper({show:e})};
