83558acd063f4f99e9d469820eaf204ea80a2cca
Shiotsuki_ <564165781@qq.com>
2022-07-29 18:00:37 +0800

    feat：cypress -> 10.3.1

--- cypress/integration/unit/convenient/generateConfig.spec.js
+++
@@ -1,8 +0,0 @@
-import { generateConfig, CONFIGTYPE } from "../../../../dist/Vis.es";
-
-describe("generateConfig", () => {
-  it("test mesh config", () => {
-    const meshConfig = generateConfig(CONFIGTYPE.MESH);
-    expect(meshConfig).to.have.property("type", CONFIGTYPE.MESH);
-  });
-});
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,6 +22,16 @@ module.exports = (on, config) => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   require("@cypress/code-coverage/task")(on, config);
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
+  // on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const webpackOptions = require("../../webpack.config");
+
+  on(
+    "file:preprocessor",
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    require("@cypress/webpack-preprocessor")({
+      webpackOptions,
+    })
+  );
   return config;
 };
