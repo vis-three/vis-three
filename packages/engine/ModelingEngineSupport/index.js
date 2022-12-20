087b43cc3365b48dc8575f49bbc96a76ffe80826
Shiotsuki_ <564165781@qq.com>
2022-12-20 17:27:24 +0800

    feat：modeling engine support

--- packages/engine/ModelingEngineSupport/index.js
+++ packages/engine/ModelingEngineSupport/index.js
@@ -1,26 +1,59 @@
-import { ENGINEPLUGIN } from "../Engine";
-import { EngineSupport } from "../EngineSupport";
+import { EngineSupport } from "@vis-three/middleware";
+import { WebGLRendererPlugin, } from "@vis-three/webgl-renderer-plugin";
+import { EffectComposerPlugin, } from "@vis-three/effect-composer-plugin";
+import { OrbitControlsPlugin, } from "@vis-three/orbit-controls-plugin";
+import { CameraAdaptivePlugin } from "@vis-three/camera-adaptive-plugin";
+import { SelectionPlugin } from "@vis-three/selection-plugin";
+import { AxesHelperPlugin, } from "@vis-three/axes-helper-plugin";
+import { GridHelperPlugin, } from "@vis-three/grid-helper-plugin";
+import { ViewpointPlugin, } from "@vis-three/viewpoint-plugin";
+import { TransformControlsPlugin, } from "@vis-three/transform-controls-plugin";
+import { StatsPlugin } from "@vis-three/stats-plugin";
+import { KeyboardManagerPlugin, } from "@vis-three/keyboard-manager-plugin";
+import { ObjectHelperPlugin, } from "@vis-three/object-helper-plugin";
+import { CSS2DRendererPlugin, } from "@vis-three/css2d-renderer-plugin";
+import { CSS3DRendererPlugin, } from "@vis-three/css3d-renderer-plugin";
+import { CSS2DRenderStrategy } from "@vis-three/css2d-render-strategy";
+import { CSS3DRenderStrategy } from "@vis-three/css3d-render-strategy";
+import { EffectRenderStrategy } from "@vis-three/effect-render-strategy";
+import { OrbitRenderStrategy } from "@vis-three/orbit-render-strategy";
+import { OrbitViewpointStrategy } from "@vis-three/orbit-viewpoint-strategy";
+import { TransSelectEventStrategy } from "@vis-three/trans-select-event-strategy";
+import { StatsRenderStrategy } from "@vis-three/stats-render-strategy";
+import { GridViewpointStrategy } from "@vis-three/grid-viewpoint-strategy";
+import { TransformKeyboardStrategy } from "@vis-three/transform-keyboard-strategy";
+import { HelperSelectInteractStrategy } from "@vis-three/helper-select-interact-strategy";
 export class ModelingEngineSupport extends EngineSupport {
-    IS_ENGINESUPPORT = true;
     constructor(parameters) {
         super(parameters);
-        this.install(ENGINEPLUGIN.WEBGLRENDERER, {
+        this.install(WebGLRendererPlugin({
             antialias: true,
             alpha: true,
-        })
-            .install(ENGINEPLUGIN.EFFECTCOMPOSER, {
+        }))
+            .install(CSS2DRendererPlugin())
+            .install(CSS3DRendererPlugin())
+            .install(EffectComposerPlugin({
             WebGLMultisampleRenderTarget: true,
-        })
-            .install(ENGINEPLUGIN.SELECTION)
-            .install(ENGINEPLUGIN.AXESHELPER)
-            .install(ENGINEPLUGIN.GRIDHELPER)
-            .install(ENGINEPLUGIN.OBJECTHELPER)
-            .install(ENGINEPLUGIN.VIEWPOINT)
-            .install(ENGINEPLUGIN.DISPLAYMODE)
-            .install(ENGINEPLUGIN.STATS)
-            .install(ENGINEPLUGIN.ORBITCONTROLS)
-            .install(ENGINEPLUGIN.KEYBOARDMANAGER)
-            .install(ENGINEPLUGIN.TRANSFORMCONTROLS)
-            .complete();
+        }))
+            .install(OrbitControlsPlugin())
+            .install(CameraAdaptivePlugin())
+            .install(SelectionPlugin())
+            .install(AxesHelperPlugin())
+            .install(GridHelperPlugin())
+            .install(ViewpointPlugin())
+            .install(TransformControlsPlugin())
+            .install(StatsPlugin())
+            .install(KeyboardManagerPlugin())
+            .install(ObjectHelperPlugin());
+        this.exec(CSS2DRenderStrategy())
+            .exec(CSS3DRenderStrategy())
+            .exec(EffectRenderStrategy())
+            .exec(OrbitRenderStrategy())
+            .exec(OrbitViewpointStrategy())
+            .exec(TransSelectEventStrategy())
+            .exec(StatsRenderStrategy())
+            .exec(GridViewpointStrategy())
+            .exec(TransformKeyboardStrategy())
+            .exec(HelperSelectInteractStrategy());
     }
 }
--- packages/engine/ModelingEngineSupport/index.ts
+++ packages/engine/ModelingEngineSupport/index.ts
@@ -1,66 +1,157 @@
-import { ENGINEPLUGIN } from "../Engine";
-import { TransformControls } from "three/examples/jsm/controls/TransformControls";
+import { EngineSupport, EngineSupportParameters } from "@vis-three/middleware";
+import {
+  AxesHelper,
+  Camera,
+  Event,
+  GridHelper,
+  Object3D,
+  Scene,
+  WebGLRenderer,
+} from "three";
 import Stats from "three/examples/jsm/libs/stats.module";
 import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
-import { EventManager } from "../manager/EventManager";
-import { PointerManager } from "../manager/PointerManager";
-import { RenderManager } from "../manager/RenderManager";
-import { Camera, Object3D, Scene, WebGLRenderer } from "three";
-import { EngineSupport, EngineSupportParameters } from "../EngineSupport";
-import { KeyboardManager } from "../manager/KeyboardManager";
-import { DISPLAYMODE } from "../../plugins/DisplayModePlugin";
-import { VIEWPOINT } from "../../plugins/ViewpointPlugin";
-import { VisOrbitControls } from "../optimize/VisOrbitControls";
+import { CSS2DRenderer } from "three/examples/jsm/renderers/CSS2DRenderer";
+import { CSS3DRenderer } from "three/examples/jsm/renderers/CSS3DRenderer";
+import {
+  Screenshot,
+  WebGLRendererEngine,
+  WebGLRendererPlugin,
+} from "@vis-three/webgl-renderer-plugin";
 
-export class ModelingEngineSupport extends EngineSupport {
-  IS_ENGINESUPPORT = true;
+import {
+  EffectComposerEngine,
+  EffectComposerPlugin,
+} from "@vis-three/effect-composer-plugin";
+import {
+  OrbitControlsEngine,
+  OrbitControlsPlugin,
+} from "@vis-three/orbit-controls-plugin";
+import { CameraAdaptivePlugin } from "@vis-three/camera-adaptive-plugin";
+import { SelectionEngine, SelectionPlugin } from "@vis-three/selection-plugin";
+import {
+  AxesHelperEngine,
+  AxesHelperOptions,
+  AxesHelperPlugin,
+} from "@vis-three/axes-helper-plugin";
+import {
+  GridHelperEngine,
+  GridHelperPlugin,
+} from "@vis-three/grid-helper-plugin";
+import {
+  VIEWPOINT,
+  ViewpointEngine,
+  ViewpointPlugin,
+} from "@vis-three/viewpoint-plugin";
+import {
+  TransformControlsEngine,
+  TransformControlsPlugin,
+  VisTransformControls,
+} from "@vis-three/transform-controls-plugin";
+import { StatsEngine, StatsPlugin } from "@vis-three/stats-plugin";
+import {
+  KeyboardManager,
+  KeyboardManagerEngine,
+  KeyboardManagerPlugin,
+} from "@vis-three/keyboard-manager-plugin";
+import {
+  ObjectHelperEngine,
+  ObjectHelperPlugin,
+  ObjectHelperManager,
+} from "@vis-three/object-helper-plugin";
+import {
+  CSS2DRendererEngine,
+  CSS2DRendererPlugin,
+} from "@vis-three/css2d-renderer-plugin";
+import {
+  CSS3DRendererEngine,
+  CSS3DRendererPlugin,
+} from "@vis-three/css3d-renderer-plugin";
 
-  declare dom: HTMLElement;
+import { CSS2DRenderStrategy } from "@vis-three/css2d-render-strategy";
+import { CSS3DRenderStrategy } from "@vis-three/css3d-render-strategy";
+import { EffectRenderStrategy } from "@vis-three/effect-render-strategy";
+import { OrbitRenderStrategy } from "@vis-three/orbit-render-strategy";
+import { OrbitViewpointStrategy } from "@vis-three/orbit-viewpoint-strategy";
+import { TransSelectEventStrategy } from "@vis-three/trans-select-event-strategy";
+import { StatsRenderStrategy } from "@vis-three/stats-render-strategy";
+import { GridViewpointStrategy } from "@vis-three/grid-viewpoint-strategy";
+import { TransformKeyboardStrategy } from "@vis-three/transform-keyboard-strategy";
+import { HelperSelectInteractStrategy } from "@vis-three/helper-select-interact-strategy";
+import { VisStats } from "@vis-three/stats-plugin/VisStats";
+export class ModelingEngineSupport
+  extends EngineSupport
+  implements
+    WebGLRendererEngine,
+    EffectComposerEngine,
+    OrbitControlsEngine,
+    KeyboardManagerEngine,
+    StatsEngine,
+    TransformControlsEngine,
+    ViewpointEngine,
+    GridHelperEngine,
+    AxesHelperEngine,
+    SelectionEngine,
+    ObjectHelperEngine,
+    CSS2DRendererEngine,
+    CSS3DRendererEngine
+{
   declare webGLRenderer: WebGLRenderer;
-  declare camera: Camera;
-  declare scene: Scene;
-  declare orbitControls: VisOrbitControls;
-  declare transformControls: TransformControls;
+  declare getScreenshot: (params?: Screenshot | undefined) => Promise<string>;
   declare effectComposer: EffectComposer;
-  declare renderManager: RenderManager;
-  declare pointerManager: PointerManager;
-  declare eventManager: EventManager;
+  declare orbitControls;
   declare keyboardManager: KeyboardManager;
-  declare stats: Stats;
-  declare displayMode: DISPLAYMODE;
-  declare selectionBox: Set<Object3D>;
-
-  declare setStats: (show: boolean) => this;
-  declare setTransformControls: (show: boolean) => this;
-  declare setViewpoint: (viewpoint: VIEWPOINT) => this;
-  declare setDisplayMode: (mode: DISPLAYMODE) => this;
-  declare setAxesHelper: (params: { show: boolean }) => this;
-  declare setGridHelper: (params: { show: boolean }) => this;
-  declare setObjectHelper: (params: { show: boolean }) => this;
-
-  declare play: () => this;
-  declare stop: () => this;
-  declare render: () => this;
+  declare stats: VisStats;
+  declare setStats: (show: boolean) => StatsEngine;
+  declare transing: boolean;
+  declare transformControls: VisTransformControls;
+  declare setTransformControls: (show: boolean) => TransformControlsEngine;
+  declare setViewpoint;
+  declare gridHelper: GridHelper;
+  declare setGridHelper: (show: boolean) => GridHelperEngine;
+  declare axesHelper: AxesHelper;
+  declare setAxesHelper: (params: AxesHelperOptions) => AxesHelperEngine;
+  declare selectionBox: Set<Object3D<Event>>;
+  declare setSelectionBox: (objects: Object3D<Event>[]) => SelectionEngine;
+  declare objectHelperManager: ObjectHelperManager;
+  declare setObjectHelper: (show: boolean) => ObjectHelperEngine;
+  declare css2DRenderer: CSS2DRenderer;
+  declare css3DRenderer: CSS3DRenderer;
 
   constructor(parameters?: EngineSupportParameters) {
     super(parameters);
-    this.install(ENGINEPLUGIN.WEBGLRENDERER, {
-      antialias: true,
-      alpha: true,
-    })
-      .install(ENGINEPLUGIN.EFFECTCOMPOSER, {
-        WebGLMultisampleRenderTarget: true,
+    this.install(
+      WebGLRendererPlugin({
+        antialias: true,
+        alpha: true,
       })
-      .install(ENGINEPLUGIN.SELECTION)
-      .install(ENGINEPLUGIN.AXESHELPER)
-      .install(ENGINEPLUGIN.GRIDHELPER)
-      .install(ENGINEPLUGIN.OBJECTHELPER)
-      .install(ENGINEPLUGIN.VIEWPOINT)
-      .install(ENGINEPLUGIN.DISPLAYMODE)
-      .install(ENGINEPLUGIN.STATS)
-      .install(ENGINEPLUGIN.ORBITCONTROLS)
-      .install(ENGINEPLUGIN.KEYBOARDMANAGER)
-      .install(ENGINEPLUGIN.TRANSFORMCONTROLS)
-      .complete();
+    )
+      .install(CSS2DRendererPlugin())
+      .install(CSS3DRendererPlugin())
+      .install(
+        EffectComposerPlugin({
+          WebGLMultisampleRenderTarget: true,
+        })
+      )
+      .install(OrbitControlsPlugin())
+      .install(CameraAdaptivePlugin())
+      .install(SelectionPlugin())
+      .install(AxesHelperPlugin())
+      .install(GridHelperPlugin())
+      .install(ViewpointPlugin())
+      .install(TransformControlsPlugin())
+      .install(StatsPlugin())
+      .install(KeyboardManagerPlugin())
+      .install(ObjectHelperPlugin());
+
+    this.exec(CSS2DRenderStrategy())
+      .exec(CSS3DRenderStrategy())
+      .exec(EffectRenderStrategy())
+      .exec(OrbitRenderStrategy())
+      .exec(OrbitViewpointStrategy())
+      .exec(TransSelectEventStrategy())
+      .exec(StatsRenderStrategy())
+      .exec(GridViewpointStrategy())
+      .exec(TransformKeyboardStrategy())
+      .exec(HelperSelectInteractStrategy());
   }
 }
