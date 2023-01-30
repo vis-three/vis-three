6192e113bd54a396f9fc800d2e6ced5149cb1bb2
Shiotsuki_ <564165781@qq.com>
2023-01-30 11:43:29 +0800

    feat：360 vr panorama demo

--- packages/middleware/utils/index.ts
+++ packages/middleware/utils/index.ts
@@ -1,4 +1,10 @@
+import { v4 } from "uuid";
+
 export * as JSONHandler from "./JSONHandler";
 export * as Template from "./Template";
 export * from "./AntiShake";
 export * from "./generateConfig";
+
+export const createSymbol = function () {
+  return v4();
+};
